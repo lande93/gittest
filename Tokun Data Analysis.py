cc2b17ed5ff997cb27de1eeec801136898d0761b
lande93 <85822022+lande93@users.noreply.github.com>
2021-06-13 09:37:59 +0100

    Add files via upload

---
+++ Tokun Data Analysis.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+import matplotlib.pyplot as plt
+
+# In[6]:
+
+
+student = np.array([3,4,6,7])
+age = np.array([10,67, 100, 150])
+plt.plot(student, age)
+plt. show()
+
+
+# In[ ]:
+
+
+
+
