135567a88a31edb3b226a93bdcb74bae7366335e
lande93 <85822022+lande93@users.noreply.github.com>
2021-06-14 20:33:32 +0100

    Add files via upload
    This is a python file

---
+++ edx-checkpoint.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('hello world')
+
+
+# In[2]:
+
+
+365*24*60*60
+
+
+# In[3]:
+
+
+_ / 1e7
+
+
+# In[4]:
+
+
+x = 4 + 3
+print(x)
+
+
+# # This is a markdown cell.
+# 
+# ###  This is heading 3
+# hello:
+# 
+# * lanre
+# * bamise
+# * tokun
+# 
+# **example text**
+# 
+# *love first*
+# 
+# http://goal.com
+
+# In[10]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+import matplotlib.pyplot as plt
+
+plot([0,1,0,1])
+plt.show()
+
+
+# **This is a plot using matplotlib of a vector**
+
+# In[ ]:
+
+
+
+
